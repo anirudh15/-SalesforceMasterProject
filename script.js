d39373b43081c39a3783724b1fbb5fc7bb06beb5
anirudh15 <anirudhashyap1@mail.com>
2021-07-01 19:12:07 +0100

    revert

--- script.js
+++ script.js
@@ -87,85 +87,77 @@ $(document).ready(function() {
   }
 return vArray2;
 }
-
-	$('#calendar').fullCalendar(
+});
+$('#calendar').fullCalendar(
       {
+        let getToday = getCurrentDate();
+        let calArray = getContactCampaign();
 
-      let getToday = getCurrentDate();
-      let calArray = getContactCampaign();
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,basicWeek,basicDay'
+        },
+        defaultDate: todayDate,
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
 
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,basicWeek,basicDay'
-			},
-			defaultDate: todayDate,
-			navLinks: true, // can click day/week names to navigate views
-			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-
-      events: [
-    				{
-    					title: 'All Day Event',
-    					start: '2016-12-01'
-    				},
-    				{
-    					title: 'Long Event',
-    					start: '2016-12-07',
-    					end: '2016-12-10'
-    				},
-    				{
-    					id: 999,
-    					title: 'Repeating Event',
-    					start: '2016-12-09T16:00:00'
-    				},
-    				{
-    					id: 999,
-    					title: 'Repeating Event',
-    					start: '2016-12-16T16:00:00'
-    				},
-    				{
-    					title: 'Conference',
-    					start: '2016-12-11',
-    					end: '2016-12-13'
-    				},
-    				{
-    					title: 'Meeting',
-    					start: '2016-12-12T10:30:00',
-    					end: '2016-12-12T12:30:00'
-    				},
-    				{
-    					title: 'Lunch',
-    					start: '2016-12-12T12:00:00'
-    				},
-    				{
-    					title: 'Meeting',
-    					start: '2016-12-12T14:30:00'
-    				},
-    				{
-    					title: 'Happy Hour',
-    					start: '2016-12-12T17:30:00'
-    				},
-    				{
-    					title: 'Dinner',
-    					start: '2016-12-12T20:00:00'
-    				},
-    				{
-    					title: 'Birthday Party',
-    					start: '2016-12-13T07:00:00'
-    				},
-    				{
-    					title: 'Click for Google',
-    					url: 'https://google.com/',
-    					start: '2016-12-28'
-    				}
-    			]
+        events: [
+              {
+                title: 'All Day Event',
+                start: '2016-12-01'
+              },
+              {
+                title: 'Long Event',
+                start: '2016-12-07',
+                end: '2016-12-10'
+              },
+              {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2016-12-09T16:00:00'
+              },
+              {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2016-12-16T16:00:00'
+              },
+              {
+                title: 'Conference',
+                start: '2016-12-11',
+                end: '2016-12-13'
+              },
+              {
+                title: 'Meeting',
+                start: '2016-12-12T10:30:00',
+                end: '2016-12-12T12:30:00'
+              },
+              {
+                title: 'Lunch',
+                start: '2016-12-12T12:00:00'
+              },
+              {
+                title: 'Meeting',
+                start: '2016-12-12T14:30:00'
+              },
+              {
+                title: 'Happy Hour',
+                start: '2016-12-12T17:30:00'
+              },
+              {
+                title: 'Dinner',
+                start: '2016-12-12T20:00:00'
+              },
+              {
+                title: 'Birthday Party',
+                start: '2016-12-13T07:00:00'
+              },
+              {
+                title: 'Click for Google',
+                url: 'https://google.com/',
+                start: '2016-12-28'
+              }
+            ]
       }
-
-    }
-
-
-
-
-    } );
-    });
+    );
