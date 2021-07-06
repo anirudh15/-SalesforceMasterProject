45a61cbc9e59e92c8bea5ee7b8439a0ac6cc2c42
anirudh15 <anirudhashyap1@mail.com>
2021-07-06 08:07:26 +0100

    trying to print to chekc if array works or not

---
+++ calendar.js
@@ -0,0 +1,113 @@
+// for (var i = 0; i < vArray2.length; i++) {
+//   let campDate = newArray[i][1];
+//   var dd = String(campDate.getDate()).padStart(2, '0');
+//   var mm = String(campDate.getMonth() + 1).padStart(2, '0');
+//   var yyyy = campDate.getFullYear();
+//   campDate = yyyy + '/' + mm + '/' + dd;
+// }
+window.onload = function () {
+    console.log(getContactCampaign());
+
+}
+ function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }
+
+  function getCurrentDate() {
+      let currentDate = new Date();
+      var dd = String(currentDate.getDate()).padStart(2, '0');
+      var mm = String(currentDate.getMonth() + 1).padStart(2, '0');
+      var yyyy = currentDate.getFullYear();
+      currentDate = yyyy + '-' + mm + '-' + dd;
+      return currentDate;
+  }
+
+  async function getToken() {
+      let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=Secureit123AYfrE3tYJC7OVZtTEg0hgDkI", {
+          method: "POST",
+          mode: 'cors',
+          headers: {
+              "Content-type": "application/json;charset=UTF-8",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+              "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+          },
+      });
+
+      let data = await response.json();
+      //transform data into array of demo like objects
+
+      return await data["access_token"];
+  }
+  async function getContactCampaign(){
+
+    let vArray = [];
+    let vArray2 = [];
+    let userEmail = getCookie("Id");
+
+      let campaignIdList = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=select+name,campaignId+from+CampaignMember+where+email="+userEmail, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Authorization": "Bearer " + await getToken()
+        }
+      });
+
+    campaignIdListResponse = await campaignIdList.json();
+    //console.log(JSON.stringify(campaignIdListResponse));
+     console.log(campaignIdListResponse);
+
+    for (let i = 0; i < campaignIdListResponse["totalSize"]; i++) {
+        vArray.push([campaignIdListResponse["records"]["name"]["campaignId"]]);
+
+    }
+    console.log((vArray));
+
+    let campaignNameList = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=select+name,StartDate+from+campaign+where+id+in+("+vArray+")", {
+      method: "GET",
+      mode: 'cors',
+      headers: {
+          "Content-type": "application/json;charset=UTF-8",
+          "Authorization": "Bearer " + await getToken()
+      }
+    });
+
+    campaignNameListResponse = await campaignNameList.json();
+    //console.log(JSON.stringify(campaignListResponse));
+    console.log(campaignNameListResponse);
+
+    for (let i = 0; i < campaignNameListResponse["totalSize"]; i++) {
+       //const campaigns = campaignNameListResponse
+      vArray2.push([campaignNameListResponse["name"]["StartDate"]]);
+
+      // let demosArray = []
+      // campaigns.forEach((campaign) => {
+      //   const eventObject = {
+      //     id: i,
+      //     content: campaign.name,
+      //     date: ,
+      //     source: ""
+      //   }
+
+        // demosArray.push(eventObject);
+      }
+console.log(vArray2);
+      // initCalender(demosArray);
+//loader-offset//
+//load-data-in-demo
+  //   }
+  // return vArray2;
+  }
--- script.js
+++ script.js
@@ -1,257 +1,281 @@
-// demo data for events
-let demo = [
-
-{
-  for (var i = 0; i < vArray2.length; i++) {
-    let campDate = newArray[i][1];
-    var dd = String(campDate.getDate()).padStart(2, '0');
-    var mm = String(campDate.getMonth() + 1).padStart(2, '0');
-    var yyyy = campDate.getFullYear();
-    campDate = yyyy + '/' + mm + '/' + dd;
-    {
-id: i,
-date: campDate,
-content: vArray2[i][0],
-source: "salesforce.com"
-}
-demo.join(",");
-
-}
-}
-
-];
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function getCurrentDate() {
-    let currentDate = new Date();
-    var dd = String(currentDate.getDate()).padStart(2, '0');
-    var mm = String(currentDate.getMonth() + 1).padStart(2, '0');
-    var yyyy = currentDate.getFullYear();
-    currentDate = yyyy + '-' + mm + '-' + dd;
-    return currentDate;
-}
-
-async function getToken() {
-    let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=Secureit123AYfrE3tYJC7OVZtTEg0hgDkI", {
-        method: "POST",
-        mode: 'cors',
-        headers: {
-            "Content-type": "application/json;charset=UTF-8",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-        },
-    });
-
-    let data = await response.json();
-    return await data["access_token"];
-}
-async function getContactCampaign(){
-
-  let vArray = [];
-  let vArray2 = [];
-  let userEmail = getCookie("Id");
-
-    let campaignIdList = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=select+name,campaignId+from+CampaignMember+where+email="+userEmail, {
-      method: "GET",
-      mode: 'cors',
-      headers: {
-          "Content-type": "application/json;charset=UTF-8",
-          "Authorization": "Bearer " + await getToken()
-      }
-    });
-
-  campaignIdListResponse = await campaignIdList.json();
-  for (let i = 0; i < campaignIdListResponse["totalSize"]; i++) {
-      vArray.push([campaignIdListResponse["records"]["name"]["campaignId"]]);
-
-  }
-
-  let campaignNameList = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=select+name,StartDate+from+campaign+where+id+in+("+vArray+")", {
-    method: "GET",
-    mode: 'cors',
-    headers: {
-        "Content-type": "application/json;charset=UTF-8",
-        "Authorization": "Bearer " + await getToken()
-    }
-  });
-
-  campaignNameListResponse = await campaignNameList.json();
-  for (let i = 0; i < campaignNameListResponse["totalSize"]; i++) {
-    vArray2.push([campaignListResponse["name"]["StartDate"]]);
-
-  }
-return vArray2;
-}
-
-
-
-let today = new Date(),
-currentMonth = today.getMonth(),
-currentYear = today.getFullYear();
-
-// array dias de la semana
-const weekdays = [
-"Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday",
-"Sunday"];
-
-// array meses
-const months = [
-"JANUARY",
-"FEBRUARY",
-"MARCH",
-"APRIL",
-"MAY",
-"JUNE",
-"JULY"
-"AUGUST",
-"SEPTEMBER",
-"OCTOBER",
-"NOVEMBER",
-"DECEMBER"];
+function initCalender(demo) {
+  // let demo = [
+  // {
+  //   id: "asdfa",
+  //   date: "2021/04/23",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "cbnbc",
+  //   date: "2021/04/15",
+  //   content: "Optimize Components",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "adfae4",
+  //   date: "2021/04/2",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "sdgsdfg",
+  //   date: "2021/04/23",
+  //   content: "Upload new assets",
+  //   source: "http://google.com" },
+  //
+  // {
+  //   id: "xdffgb",
+  //   date: "2021/03/10",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "asdf3a",
+  //   date: "2021/04/2",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "cbnbcr",
+  //   date: "2021/04/4",
+  //   content: "Optimize Components",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "adfae34",
+  //   date: "2021/04/5",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "sdgsd3fg",
+  //   date: "2021/04/6",
+  //   content: "Upload new assets",
+  //   source: "http://google.com" },
+  //
+  // {
+  //   id: "xdff4gb",
+  //   date: "2021/03/9",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  //
+  // {
+  //   id: "ghjku",
+  //   date: "2021/05/23",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "vnmj",
+  //   date: "2021/05/15",
+  //   content: "Optimize Components",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "sfgr",
+  //   date: "2021/06/2",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "sdgsdfg",
+  //   date: "2021/03/23",
+  //   content: "Upload new assets",
+  //   source: "http://google.com" },
+  //
+  // {
+  //   id: "gjgjj",
+  //   date: "2021/02/10",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "xvblk",
+  //   date: "2021/06/2",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "zcxv5",
+  //   date: "2021/05/4",
+  //   content: "Optimize Components",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "xvbm5",
+  //   date: "2021/03/5",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" },
+  //
+  // {
+  //   id: "sdgsd34fg",
+  //   date: "2021/07/6",
+  //   content: "Upload new assets",
+  //   source: "http://google.com" },
+  //
+  // {
+  //   id: "ader",
+  //   date: "2021/08/9",
+  //   content: "Yo have a new session",
+  //   source: "http://example.com" }];
 
 
-// structure
-let structureCalendar = createElement("div", window.root, {
-  id: "structureCalendar" }),
 
-// header
-calendarHeader = createElement("header", structureCalendar, {}),
-// header columns left center and right
-headerLeft = createElement("div", calendarHeader, { className: "left" }),
-headerCenter = createElement("div", calendarHeader, { className: "center" }),
-headerRight = createElement("div", calendarHeader, { className: "right" }),
-// inside left column
-buttonPrev = createElement("button", headerLeft, { textContent: "back" }),
-buttonNext = createElement("button", headerLeft, { textContent: "forward" }),
-centerTitle = createElement("h1", headerCenter, {
-  textContent: months[currentMonth] + " " + currentYear }),
+  let today = new Date(),
+  currentMonth = today.getMonth(),
+  currentYear = today.getFullYear();
 
-// calendar body
-calendarBody = createElement("div", structureCalendar, { id: "calendar" }),
-weekdayBody = createElement("ul", calendarBody, { id: "weekdays" }),
-daysBody = createElement("ul", calendarBody, { id: "days" });
+  // array dias de la semana
+  const weekdays = [
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  "Domingo"];
 
-// init calendar
-showCalendar(currentMonth, currentYear);
+  // array meses
+  const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre"];
 
-// map week days
-weekdays.map((item, i) =>
-// change to monday
-today.getDay() - 1 == i ?
-createElement("li", weekdayBody, { className: "today", textContent: item }) :
-createElement("li", weekdayBody, { textContent: item }));
 
+  // structure
+  let structureCalendar = createElement("div", window.root, {
+    id: "structureCalendar" }),
 
-// buttons next prev
-buttonPrev.onclick = () => prev();
-buttonNext.onclick = () => next();
+  // header
+  calendarHeader = createElement("header", structureCalendar, {}),
+  // header columns left center and right
+  headerLeft = createElement("div", calendarHeader, { className: "left" }),
+  headerCenter = createElement("div", calendarHeader, { className: "center" }),
+  headerRight = createElement("div", calendarHeader, { className: "right" }),
+  // inside left column
+  buttonPrev = createElement("button", headerLeft, { textContent: "Anterior" }),
+  buttonNext = createElement("button", headerLeft, { textContent: "Siguiente" }),
+  centerTitle = createElement("h1", headerCenter, {
+    textContent: months[currentMonth] + " " + currentYear }),
 
-// generate calendar
-function showCalendar(month, year) {
-  // first day - 1
-  let firstDay = new Date(year, month).getDay() - 1;
+  // calendar body
+  calendarBody = createElement("div", structureCalendar, { id: "calendar" }),
+  weekdayBody = createElement("ul", calendarBody, { id: "weekdays" }),
+  daysBody = createElement("ul", calendarBody, { id: "days" });
 
-  // clear preview content
-  daysBody.textContent = "";
-
-  // filing data about month and in the page via DOM.
-  centerTitle.textContent = months[month] + " " + year;
-
-  // creating all cells
-  let date = 1;
-  for (let i = 0; i < 6; i++) {
-    //creating individual cells, filing them up with data.
-    for (let j = 0; j < 7; j++) {
-      if (i === 0 && j < firstDay) {
-        createElement("li", daysBody, { textContent: "" });
-      } else if (date > daysInMonth(month, year)) {
-        break;
-      } else {
-        let li = createElement("li", daysBody, {}),
-        info = createElement("div", li, {
-          className: "info",
-          textContent: weekdays[j] }),
-
-        div = createElement("div", li, { className: "date", textContent: date });
-        // ----------------------------
-        // ----- view events
-        if (typeof demo !== "undefined") {
-          viewEvents(demo, li, [year, month, date]);
-        }
-        // ----------------------------
-        if (
-        date === today.getDate() &&
-        year === today.getFullYear() &&
-        month === today.getMonth())
-        {
-          li.className = "today";
-        }
-        date++;
-      }
-    }
-  }
-}
-// view events
-function viewEvents(data, where, args) {
-  return (
-    data &&
-    data.map(item => {
-      let date = item.date.split("/"),
-      year = parseInt(date[0]),
-      month = parseInt(date[1]) - 1,
-      day = parseInt(date[2]);
-
-      if (year === args[0] && month === args[1] && day === args[2]) {
-        let event = createElement("div", where, { className: "ev", id: item.id }),
-        eventDesc = createElement("div", event, { className: "ev-desc" });
-        eventDesc.innerHTML = `<a href="${item.source}">${item.content}</a>`;
-        event.onclick = () => alert(eventDesc.textContent);
-      }
-    }));
-
-}
-
-// next month
-function next() {
-  currentMonth = (currentMonth + 1) % 12;
-  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+  // init calendar
   showCalendar(currentMonth, currentYear);
-}
-// previus month
-function prev() {
-  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
-  showCalendar(currentMonth, currentYear);
-}
-// check how many days in a month code from
-// https://dzone.com/articles/determining-number-days-month
-function daysInMonth(iMonth, iYear) {
-  return 32 - new Date(iYear, iMonth, 32).getDate();
-}
-// --- Create element
-function createElement(element, where, args) {
-  let d = document.createElement(element);
-  if (args) for (const [k, v] of Object.entries(args)) d[k] = v;
-  where.appendChild(d);
-  return d;
+
+  // map week days
+  weekdays.map((item, i) =>
+  // change to monday
+  today.getDay() - 1 == i ?
+  createElement("li", weekdayBody, { className: "today", textContent: item }) :
+  createElement("li", weekdayBody, { textContent: item }));
+
+
+  // buttons next prev
+  buttonPrev.onclick = () => prev();
+  buttonNext.onclick = () => next();
+
+  // generate calendar
+  function showCalendar(month, year) {
+    // first day - 1
+    let firstDay = new Date(year, month).getDay() - 1;
+
+    // clear preview content
+    daysBody.textContent = "";
+
+    // filing data about month and in the page via DOM.
+    centerTitle.textContent = months[month] + " " + year;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+      //creating individual cells, filing them up with data.
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < firstDay) {
+          createElement("li", daysBody, { textContent: "" });
+        } else if (date > daysInMonth(month, year)) {
+          break;
+        } else {
+          let li = createElement("li", daysBody, {}),
+          info = createElement("div", li, {
+            className: "info",
+            textContent: weekdays[j] }),
+
+          div = createElement("div", li, { className: "date", textContent: date });
+          // ----------------------------
+          // ----- view events
+          if (typeof demo !== "undefined") {
+            viewEvents(demo, li, [year, month, date]);
+          }
+          // ----------------------------
+          if (
+          date === today.getDate() &&
+          year === today.getFullYear() &&
+          month === today.getMonth())
+          {
+            li.className = "today";
+          }
+          date++;
+        }
+      }
+    }
+  }
+  // view events
+  function viewEvents(data, where, args) {
+    return (
+      data &&
+      data.map(item => {
+        let date = item.date.split("/"),
+        year = parseInt(date[0]),
+        month = parseInt(date[1]) - 1,
+        day = parseInt(date[2]);
+
+        if (year === args[0] && month === args[1] && day === args[2]) {
+          let event = createElement("div", where, { className: "ev", id: item.id }),
+          eventDesc = createElement("div", event, { className: "ev-desc" });
+          eventDesc.innerHTML = `<a href="${item.source}">${item.content}</a>`;
+          event.onclick = () => alert(eventDesc.textContent);
+        }
+      }));
+
+  }
+
+  // next month
+  function next() {
+    currentMonth = (currentMonth + 1) % 12;
+    currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+    showCalendar(currentMonth, currentYear);
+  }
+  // previus month
+  function prev() {
+    currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+    currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+    showCalendar(currentMonth, currentYear);
+  }
+  // check how many days in a month code from
+  // https://dzone.com/articles/determining-number-days-month
+  function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+  }
+  // --- Create element
+  function createElement(element, where, args) {
+    let d = document.createElement(element);
+    if (args) for (const [k, v] of Object.entries(args)) d[k] = v;
+    where.appendChild(d);
+    return d;
+  }
 }
